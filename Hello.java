50387400fd79318a50887b90e5b517dd3349154c
Gigaseui <sphemanth8@gmail.com>
2025-07-05 21:38:04 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 class Hello{
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Automated Build Trigger /1min in Jenkins if file commit changes");
     }
 }
